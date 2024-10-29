3c8686cb11ae7d1ba0bd6ad757d8ad36f5d5afd8
Malvin King <malvink003@gmail.com>
2024-10-29 19:55:33 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -8,7 +8,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Malvin-MD~LcBFWbcH",
+  SESSION_ID: process.env.SESSION_ID || "MALVIN-MD&1AAJtwpw",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
